453ca518adfbc91795fbbdddeb9fce1b57bebebe
Tomasz Lisowski <Tomasz.Lisowski@endava.com>
2022-12-14 10:46:37 +0100

    Initial commit

---
+++ src/main/java/com/tomlisow/githubapi/GitHubApiApplication.java
@@ -0,0 +1,13 @@
+package com.tomlisow.githubapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubApiApplication.class, args);
+    }
+
+}
