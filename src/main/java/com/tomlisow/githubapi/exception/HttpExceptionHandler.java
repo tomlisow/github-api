567831dac8632b04141df92d0061870f095348df
Tomasz Lisowski <Tomasz.Lisowski@endava.com>
2022-12-14 14:00:49 +0100

    ExceptionHandler added for: HttpClientErrorException and HttpMediaTypeNotAcceptableException

---
+++ src/main/java/com/tomlisow/githubapi/exception/HttpExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.tomlisow.githubapi.exception;
+
+import com.tomlisow.githubapi.model.ErrorResponse;
+import org.springframework.http.*;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class HttpExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(HttpClientErrorException.class)
+    public ResponseEntity<ErrorResponse> handleHttpClientErrorException() {
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                "Non-existent GitHub username");
+        return ResponseEntity
+                .status(errorResponse.status())
+                .body(errorResponse);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMediaTypeNotAcceptable(HttpMediaTypeNotAcceptableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.NOT_ACCEPTABLE.value(),
+                "Wrong HTTP Media Type. Acceptable representations: [application/json].");
+        return ResponseEntity
+                .status(errorResponse.status())
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(errorResponse);
+    }
+}
